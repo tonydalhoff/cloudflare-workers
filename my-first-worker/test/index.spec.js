26881238f792ac989f572b641ccd833e2bf88a95
Tony Dalhoff <tdalhoff09@gmail.com>
2025-06-15 18:17:43 -0600

    Update test stuff

--- my-first-worker/test/index.spec.js
+++ my-first-worker/test/index.spec.js
@@ -2,19 +2,19 @@ import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloud
 import { describe, it, expect } from 'vitest';
 import worker from '../src';
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
+describe('Hello Worker worker', () => {
+	it('responds with Hello Worker! (unit style)', async () => {
 		const request = new Request('http://example.com');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"Hello Worker!"`);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
+	it('responds with Hello Worker! (integration style)', async () => {
 		const response = await SELF.fetch('http://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"Hello Worker!"`);
 	});
 });
